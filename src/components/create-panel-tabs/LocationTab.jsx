27afe637e616d7fbd8fc49392d3d802cb82874ef
Ivana Krstic <ivanak1996@gmail.com>
2022-10-02 23:41:51 +0200

    mapa koja radi za sad

--- src/components/create-panel-tabs/LocationTab.jsx
+++ src/components/create-panel-tabs/LocationTab.jsx
@@ -4,6 +4,17 @@ import Input from "../core/Input";
 import Select from "../core/Select";
 import { useState } from "react";
 import { useCallback } from "react";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import {compose, withProps} from "recompose"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 44.786568, lng: 20.448921 }}
+    >
+        {props.isMarkerShown && <Marker position={{ lat: 44.786568, lng: 20.448921 }} />}
+    </GoogleMap>
+))
 
 const LocationTab = (props) => {
     const { setIndoorLocation, indoorLocation, setCountry, country, setCity, city, setAddress, address, advertisingType, setAdvertisingType, setType, type, setLongitude, setLatitude, longitude, latitude } = props;
@@ -34,7 +45,13 @@ const LocationTab = (props) => {
                 <button className="white-button ml-s" onClick={applyCoordinates}>Apply coordinates</button>
             </div>
             <div class="location-tab-right">
-                
+                <MyMapComponent
+                    isMarkerShown
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
             </div>
         </div>
     )
--- src/components/panel-search/PanelSearch.jsx
+++ src/components/panel-search/PanelSearch.jsx
@@ -3,69 +3,23 @@ import "../../css/PanelSearch.css"
 import CircleImageWithText from "../core/CircleImageWithText";
 import Select from "../core/Select";
 import Pagination from '../core/Pagination';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 51.507351, lng: -0.127758 }}
+    >
+        {panels.map((panel) => {
+            return <Marker key={panel.id} position={{ lat: panel.lat, lng: panel.lng }} />
+        }
+        )}
+    </GoogleMap>
+))
 
 const PanelSearch = (props) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage, setPostsPerPage] = useState(5);
-
-    const panels = [
-        {
-            id: 1,
-            imgUrl: "https://st.depositphotos.com/1004061/2699/i/950/depositphotos_26992121-stock-photo-london-the-uk-red-bus.jpg",
-            isAvailable: true,
-            address: "Westminster Road 878 Ldn",
-            panelName: "Panel u Londonu"
-        },
-        {
-            id: 2,
-            imgUrl: "https://www.aparisguide.com/eiffeltower/eiffel-tower003.jpg",
-            isAvailable: false,
-            address: "la Rue de quelque chose 18",
-            panelName: "Panel u Parizu"
-        },
-        {
-            id: 3,
-            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/7/70/Ginza_at_Night%2C_Tokyo.jpg",
-            isAvailable: true,
-            address: "Sakura Avenue 12",
-            panelName: "Panel u Tokiju"
-        },
-        {
-            id: 4,
-            imgUrl: "https://www.travellens.co/content/images/2019/09/milan-italy.jpg",
-            isAvailable: true,
-            address: "Piazza Fontana 3",
-            panelName: "Panel u Milanu"
-        },
-        {
-            id: 5,
-            imgUrl: "https://rivierabarcrawltours.com/wp-content/uploads/2019/07/What-to-visit-in-Nice-France-3.jpg",
-            isAvailable: true,
-            address: "Avenue d'Azure 65",
-            panelName: "Panel u Nici"
-        },
-        {
-            id: 6,
-            imgUrl: "https://a.cdn-hotels.com/gdcs/production73/d1723/35b8f7e3-14c4-4d53-ae2f-5f7f6adb6aac.jpg",
-            isAvailable: false,
-            address: "Scottish Alley 67",
-            panelName: "Panel u Edinburgu"
-        },
-        {
-            id: 7,
-            imgUrl: "https://www.livemoretravelmore.com/wp-content/uploads/2020/07/14625650018_fe95e6264a_c.jpg",
-            isAvailable: true,
-            address: "Monsoon Road 12",
-            panelName: "Panel u Maleu"
-        },
-        {
-            id: 8,
-            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/5/50/Prague_skyline_view.jpg",
-            isAvailable: true,
-            address: "Beer Square 54",
-            panelName: "Panel u Pragu"
-        }
-    ]
 
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -98,10 +52,91 @@ const PanelSearch = (props) => {
                     />
                 </div>
                 <div class="panel-search-right">
-
+                    <MyMapComponent
+                        isMarkerShown
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDx_ilNDGwwum-bg0408T4dF5LNsCidu9c"
+                        loadingElement={<div style={{ height: `100%` }} />}
+                        containerElement={<div style={{ height: `100%` }} />}
+                        mapElement={<div style={{ height: `100%` }} />}
+                    />
                 </div>
             </div>
         </div>)
 }
+
+const panels = [
+    {
+        id: 1,
+        imgUrl: "https://st.depositphotos.com/1004061/2699/i/950/depositphotos_26992121-stock-photo-london-the-uk-red-bus.jpg",
+        isAvailable: true,
+        address: "Westminster Road 878 Ldn",
+        panelName: "Panel u Londonu",
+        lat: 51.507351,
+        lng: -0.127758
+    },
+    {
+        id: 2,
+        imgUrl: "https://www.aparisguide.com/eiffeltower/eiffel-tower003.jpg",
+        isAvailable: false,
+        address: "la Rue de quelque chose 18",
+        panelName: "Panel u Parizu",
+        lat: 48.864716,
+        lng: 2.349014
+    },
+    {
+        id: 3,
+        imgUrl: "https://upload.wikimedia.org/wikipedia/commons/7/70/Ginza_at_Night%2C_Tokyo.jpg",
+        isAvailable: true,
+        address: "Sakura Avenue 12",
+        panelName: "Panel u Tokiju",
+        lat: 35.652832,
+        lng: 139.839478
+    },
+    {
+        id: 4,
+        imgUrl: "https://www.travellens.co/content/images/2019/09/milan-italy.jpg",
+        isAvailable: true,
+        address: "Piazza Fontana 3",
+        panelName: "Panel u Milanu",
+        lat: 45.464664,
+        lng: 9.188540
+    },
+    {
+        id: 5,
+        imgUrl: "https://rivierabarcrawltours.com/wp-content/uploads/2019/07/What-to-visit-in-Nice-France-3.jpg",
+        isAvailable: true,
+        address: "Avenue d'Azure 65",
+        panelName: "Panel u Nici",
+        lat: 43.675819,
+        lng: 7.289429
+    },
+    {
+        id: 6,
+        imgUrl: "https://a.cdn-hotels.com/gdcs/production73/d1723/35b8f7e3-14c4-4d53-ae2f-5f7f6adb6aac.jpg",
+        isAvailable: false,
+        address: "Scottish Alley 67",
+        panelName: "Panel u Edinburgu",
+        lat: 55.953251,
+        lng: -3.188267
+    },
+    {
+        id: 7,
+        imgUrl: "https://www.livemoretravelmore.com/wp-content/uploads/2020/07/14625650018_fe95e6264a_c.jpg",
+        isAvailable: true,
+        address: "Monsoon Road 12",
+        panelName: "Panel u Maleu",
+        lat: 4.175496,
+        lng: 73.509346
+    },
+    {
+        id: 8,
+        imgUrl: "https://upload.wikimedia.org/wikipedia/commons/5/50/Prague_skyline_view.jpg",
+        isAvailable: true,
+        address: "Beer Square 54",
+        panelName: "Panel u Pragu",
+        lat: 50.075539,
+        lng: 14.437800
+    }
+]
 
 export default PanelSearch;
